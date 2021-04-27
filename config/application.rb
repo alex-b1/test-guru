c4038f62e4319bfab8b012c2e109e50504f2fca0
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-04-27 22:32:44 +0000

    part2

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Moscow'
+
+    # Locale configuration
+    config.i18n.available_locales = %i[ru en]
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
