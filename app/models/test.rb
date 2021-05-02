66cf4680591dc5cd7c807656f341a67da2bb6e7c
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-05-02 18:54:12 +0000

    fix add author

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :completed_tests
   has_many :users, through: :completed_tests
   belongs_to :category
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   def self.list_names(category)
     joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :completed_tests
-  has_many :tests
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :completed_tests
 
   def test_by_level(level)
