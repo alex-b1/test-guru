c3393234d190992bc58f853a0e8b02a1e7083431
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-05-07 15:41:56 +0000

    feature/part9

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def question_header(item)
-    item..persisted? ? "Редактировать вопрос #{item.body}" : 'Создать новый вопрос'
+    item.persisted? ? "Редактировать вопрос #{item.body}" : 'Создать новый вопрос'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  validates :name, :email presence: true
+  validates :name, :email, presence: true
 
-  has_many :completed_tests
+  has_many :test_passages
   has_many :author_tests, class_name: 'Test', foreign_key: :author_id
-  has_many :tests, through: :completed_tests
+  has_many :tests, through: :test_passages
 
   def test_by_level(level)
     tests.where(level: level)
+  end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
   end
 end
