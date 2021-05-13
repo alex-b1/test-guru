34ab48f95ca6e045d3dcf78e890a7405a5c139e1
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-05-13 11:53:24 +0000

    feature/part13

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'rails-i18n', '~> 6.0.0'
 
 # AUTH
+#
 
 gem 'devise', '~> 4.0'
 
--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,7 +1,18 @@
 module FlashHelper
+  ALERT_MAP = {
+      notice: 'info',
+      error: 'danger',
+      alert: 'secondary',
+  }
+
   def flash_message(type)
     if flash[type]
       content_tag :p, flash[:alert], class: "flash #{type}"
     end
   end
+
+  def alert_map(key)
+    ALERT_MAP[key.to_sym] || key
+  end
+
 end
